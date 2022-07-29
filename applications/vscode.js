1cff9a079c9ee552a6093795d695fab819fbe22e
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2022-07-29 14:51:24 +0200

    vscode: shorten reference command

--- applications/vscode.js
+++ applications/vscode.js
@@ -3,14 +3,14 @@ const { pause } = require('../utils')
 const codeShortcutMap = {
   bookmark: ['k', ['commandOrControl', 'shift']], // requires bookmark extension and changing its shortcut
   definition: ['f12'],
-  'find references': ['f12', ['alt', 'shift']],
+  references: ['f12', ['alt', 'shift']],
   'format document': ['f', ['alt', 'option', 'shift']],
   'organize imports': ['o', ['alt', 'shift']],
   'word wrap': ['z', ['alt']],
 }
 Object.keys(codeShortcutMap).forEach((shortcut) => {
-  serenade.app('code').command(shortcut, (api) => {
-    api.pressKey.apply(null, codeShortcutMap[shortcut])
+  serenade.app('code').command(shortcut, async (api) => {
+    await api.pressKey(...codeShortcutMap[shortcut])
   })
 })
 
@@ -29,6 +29,10 @@ serenade
     await api.typeText(matches.snippetName)
     await api.pressKey('enter')
   })
+
+serenade.app('code').command('file references', async (api) => {
+  await runPaletteCommand(api, 'typescript.findAllFileReferences')
+})
 
 serenade.app('code').command('restart typescript', async (api) => {
   await runPaletteCommand(api, 'typescript.restartTsServer')
