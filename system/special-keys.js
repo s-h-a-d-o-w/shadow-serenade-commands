761177c4aeccf11c667c710e141583a1eef8d6ca
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2022-07-29 14:50:13 +0200

    Add short key commands for special characters

--- system/special-keys.js
+++ system/special-keys.js
@@ -1,49 +1,67 @@
 const { isMac } = require('../utils/consts')
 
-const mappedSpecialKeys = {
+const shortcutMap = {
   'alt tab': ['tab', ['alt']],
-  ampersand: ['&'],
-  at: ['@'],
-  bang: ['!'],
-  backtick: ['`'],
-  backslash: ['\\'],
-  colon: [':'],
-  comma: [','],
-  dash: ['-'],
-  dot: ['.'],
-  'double quote': ['"'],
-  end: ['end'],
-  equals: ['='],
-  'exclamation point': ['!'],
-  'greater than': ['>'],
-  hash: ['#'],
-  home: ['home'],
-  'left brace': ['{'],
-  'left bracket': ['['],
-  'less than': ['<'],
-  minus: ['-'],
-  percent: ['%'],
-  period: ['.'],
-  pipe: ['|'],
-  plus: ['+'],
-  'question mark': ['?'],
-  quote: ['"'],
-  'right brace': ['}'],
-  'right bracket': [']'],
-  semi: [';'],
-  star: ['*'],
-  'single quote': ["'"],
-  slash: ['/'],
   switch: ['tab', ['alt']],
-  tilde: ['~'],
-  times: ['*'],
   toggle: ['tab', ['control']],
-  underscore: ['_'],
 }
-Object.keys(mappedSpecialKeys).forEach((shortcut) => {
+Object.keys(shortcutMap).forEach((shortcut) => {
   serenade.global().command(shortcut, (api) => {
-    api.pressKey.apply(null, mappedSpecialKeys[shortcut])
+    api.pressKey.apply(null, shortcutMap[shortcut])
   })
+})
+
+const mappedSpecialCharacters = {
+  ampersand: '&',
+  at: '@',
+  bang: '!',
+  backtick: '`',
+  backslash: '\\',
+  colon: ':',
+  comma: ',',
+  dash: '-',
+  dot: '.',
+  'double quote': '"',
+  end: 'end',
+  equals: '=',
+  'exclamation point': '!',
+  'greater than': '>',
+  hash: '#',
+  home: 'home',
+  'left brace': '{',
+  'left bracket': '[',
+  'less than': '<',
+  minus: '-',
+  percent: '%',
+  period: '.',
+  pipe: '|',
+  plus: '+',
+  'question mark': '?',
+  quote: '"',
+  'right brace': '}',
+  'right bracket': ']',
+  semi: ';',
+  star: '*',
+  'single quote': "'",
+  slash: '/',
+  tilde: '~',
+  times: '*',
+  underscore: '_',
+}
+Object.keys(mappedSpecialCharacters).forEach((character) => {
+  serenade.global().command(character, (api) => {
+    api.pressKey(mappedSpecialCharacters[character])
+  })
+
+  if (isMac) {
+    serenade.global().command('command ' + character, (api) => {
+      api.pressKey(mappedSpecialCharacters[character], ['command'])
+    })
+  } else {
+    serenade.global().command('control ' + character, (api) => {
+      api.pressKey(mappedSpecialCharacters[character], ['control'])
+    })
+  }
 })
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
