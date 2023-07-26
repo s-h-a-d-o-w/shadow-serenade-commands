799b8575364a1d5f1847474e15f800b855fb2024
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2023-07-26 14:22:13 +0200

    git: add move

--- user-specific/git.js
+++ user-specific/git.js
@@ -1,11 +1,11 @@
 const { addGlobalCommands } = require('../utils')
 
 addGlobalCommands({
-  'pull': async (api) => {
+  pull: async (api) => {
     await api.typeText('git pull')
     await api.pressKey('enter')
   },
-  'push': async (api) => {
+  push: async (api) => {
     await api.typeText('git push')
     await api.pressKey('enter')
   },
@@ -13,7 +13,7 @@ addGlobalCommands({
     await api.typeText('git push --force')
     await api.pressKey('enter')
   },
-  'commit': async (api) => {
+  commit: async (api) => {
     await api.typeText('git commit')
     await api.pressKey('enter')
   },
@@ -21,12 +21,15 @@ addGlobalCommands({
     await api.typeText('git commit --no-verify')
     await api.pressKey('enter')
   },
-  'amend': async (api) => {
+  amend: async (api) => {
     await api.typeText('git commit --amend')
     await api.pressKey('enter')
   },
   'amend no verify': async (api) => {
     await api.typeText('git commit --amend --no-verify')
     await api.pressKey('enter')
+  },
+  move: (api) => {
+    api.typeText('git mv ')
   },
 })
