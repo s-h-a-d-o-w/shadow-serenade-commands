9e702262c3454497960d0c65bf6fa0a942d8a55c
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-11-06 14:33:42 +0100

    "Remove" hints

--- user-specific/hints.js
+++ user-specific/hints.js
@@ -1,5 +1,8 @@
-const hints = ['verdaccio']
+// NOTE: Just put these into `words.json` instead.
+// (I'm keeping this around as a reminder to my future self, in case I'm tempted to do this ever again.)
 
-hints.forEach((hint) => {
-  serenade.global().hint(hint)
-})
+// const hints = ['verdaccio']
+
+// hints.forEach((hint) => {
+//   serenade.global().hint(hint)
+// })
