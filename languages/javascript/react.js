a049accb1e9335b489490d1a702ea2dc269d3cbb
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2025-07-27 13:39:15 +0200

    Add react commands

--- languages/javascript/react.js
+++ languages/javascript/react.js
@@ -1,15 +1,53 @@
 serenade
   .language('javascript')
-  .snippet('add use effect', 'useEffect(() => {\n  <%cursor%>\n}, []);')
+  .snippet('add import react', 'import React from "react";', {}, 'import')
+
+new Array('callback', 'effect', 'memo', 'layout effect').forEach((hook) => {
+  serenade
+    .language('javascript')
+    .snippet(
+      `use ${hook}`,
+      `use${
+        hook === 'layout effect'
+          ? 'LayoutEffect'
+          : hook.charAt(0).toUpperCase() + hook.slice(1)
+      }(() => {\n<%indent%><%cursor%>\n}, []);`
+    )
+})
 
 serenade
   .language('javascript')
-  .snippet('add import react', 'import React from "react";', {}, 'import')
+  .snippet(
+    'use action state <%name%>',
+    'const [state, formAction, isPending] = useActionState(<%name%>, <%cursor%>);'
+  )
 
-// See https://github.com/serenadeai/custom-commands/issues/13
-// serenade
-//   .language("javascript")
-//   .snippet(
-//     "add state <%getter%>",
-//     "const [<%getter:camel%>, set<%getter:pascal%>] = useState(null);",
-//   );
+serenade
+  .language('javascript')
+  .snippet('use context <%name%>', 'const {<%cursor%>} = useContext(<%name%>);')
+
+serenade
+  .language('javascript')
+  .snippet(
+    'use deferred <%name%>',
+    'const <%name%> = useDeferredValue(<%cursor%>);'
+  )
+
+serenade
+  .language('javascript')
+  .snippet('use ref <%name%>', 'const <%name%> = useRef(null);')
+
+// Requires manual fixing (setter casing) after use
+serenade
+  .language('javascript')
+  .snippet(
+    'use state <%name%>',
+    'const [<%name%>, set<%name%>] = useState(<%cursor%>);'
+  )
+
+serenade
+  .language('javascript')
+  .snippet(
+    'use transition',
+    'const [isPending, startTransition] = useTransition();'
+  )
