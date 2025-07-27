f87d917e8be79eb8407693522b59de7c224a0951
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2025-07-27 13:39:56 +0200

    Tweak remove directory commands

--- system/shells.js
+++ system/shells.js
@@ -1,12 +1,23 @@
 // Various shell commands for both Linux and Windows.
 // Usually no platform detection because of WSL.
 const { addGlobalCommands } = require('../utils')
+const { isWindows } = require('../utils/consts')
 
 addGlobalCommands({
-  'remove directory linux': async (api) => {
+  'rem deer': async (api) => {
+    if (isWindows) {
+      await api.typeText('Remove-Item -Recurse -Force ')
+    } else {
+      await api.typeText('rm -rf ')
+    }
+  },
+  'rem deer linux': async (api) => {
     await api.typeText('rm -rf ')
   },
-  'remove directory windows': async (api) => {
+  'rem deer power': async (api) => {
+    await api.typeText('Remove-Item -Recurse -Force ')
+  },
+  'rem deer windows': async (api) => {
     await api.typeText('rmdir /S /Q ')
   },
 })
