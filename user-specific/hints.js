168c2ce8a07b2e9279cc90f7f32ee1a254423b2c
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2025-09-28 15:17:36 +0200

    Delete user specific code

--- user-specific/hints.js
+++
@@ -1,8 +0,0 @@
-// NOTE: Just put these into `words.json` instead.
-// (I'm keeping this around as a reminder to my future self, in case I'm tempted to do this ever again.)
-
-// const hints = ['verdaccio']
-
-// hints.forEach((hint) => {
-//   serenade.global().hint(hint)
-// })
--- user-specific/names.js
+++
@@ -1,6 +0,0 @@
-const names = { nature: 'Nizar' }
-Object.keys(names).forEach((name) => {
-  serenade.global().command(`name ${name}`, (api) => {
-    api.typeText(names[name])
-  })
-})
